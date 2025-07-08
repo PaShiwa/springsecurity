73c617a3351c1eb8c0648071d8719b54e83f0547
PawShiwa <pawanshiwakot45@gmail.com>
2025-07-07 18:44:45 -0700

    feat(Add): add AuthenticationManager bean and verify method for user login by Configuring AuthenticationManager via Spring bean, Implemented verify() method using authenticationManager.authenticate(), Enabled credential-based authentication check for Users

--- src/main/java/com/pawan/SpringSecurity/config/SecurityConfig.java
+++ src/main/java/com/pawan/SpringSecurity/config/SecurityConfig.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cglib.proxy.NoOp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
@@ -42,8 +44,11 @@ public class SecurityConfig {
 //        };
         return httpSecurity
                 .csrf(customizer->customizer.disable())
-                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults())
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("register","login")
+                        .permitAll()
+                        .anyRequest().authenticated())
+//                .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
@@ -82,6 +87,11 @@ public class SecurityConfig {
         provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
         provider.setUserDetailsService(userDetailsService);
         return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
 }
--- src/main/java/com/pawan/SpringSecurity/service/UserService.java
+++ src/main/java/com/pawan/SpringSecurity/service/UserService.java
@@ -3,6 +3,9 @@ package com.pawan.SpringSecurity.service;
 import com.pawan.SpringSecurity.model.Users;
 import com.pawan.SpringSecurity.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -13,6 +16,9 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    AuthenticationManager authenticationManager;
+
     // strength-> number of hashes
     private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
 
@@ -21,4 +27,14 @@ public class UserService {
       return userRepository.save(user);
 
    }
+
+    public String verify(Users user) {
+        Authentication authentication =
+                authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                        user.getUsername(),user.getPassword()));
+        if(authentication.isAuthenticated()) {
+            return "Success!";
+        }
+        return "failure!";
+    }
 }
